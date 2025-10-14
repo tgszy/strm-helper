75086cfc363d2d4806888af6ea466e0685453e30
tgszy <sunjinyuanqwe@gmail.com>
2025-10-14 09:05:06 +0800

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-menu mode="horizontal" :router="true">
+        <el-menu-item index="/">首页</el-menu-item>
+        <el-menu-item index="/organize">整理</el-menu-item>
+        <el-menu-item index="/settings">配置</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <router-view />
+    </el-main>
+  </el-container>
+</template>
