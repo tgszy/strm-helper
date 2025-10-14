e8d87666f9c656956f1a18553bbf90a41aab35e8
tgszy <sunjinyuanqwe@gmail.com>
2025-10-14 09:02:49 +0800

    Create main.ts

---
+++ frontend/src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import router from './router'
+import App from './App.vue'
+
+const pinia = createPinia().use(piniaPluginPersistedstate)
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(pinia).use(router).use(ElementPlus).mount('#app')
