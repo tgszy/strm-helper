b5ce89a87a22f648b6f5e53d5e893c08d78b74fb
tgszy <sunjinyuanqwe@gmail.com>
2025-10-14 09:07:17 +0800

    Update VersionCard.vue

--- frontend/src/components/VersionCard.vue
+++ frontend/src/components/VersionCard.vue
@@ -1,1 +1,19 @@
+<template>
+  <el-card shadow="hover">
+    <template #header>
+      <span>{{ media.name }} ({{ media.year }})</span>
+      <el-tag style="float: right">{{ media.type }}</el-tag>
+    </template>
+    <div v-for="v in media.versions" :key="v" class="version-item">
+      <el-button size="small" type="primary" plain>{{ v }}</el-button>
+    </div>
+  </el-card>
+</template>
 
+<script setup lang="ts">
+defineProps<{ media: any }>()
+</script>
+
+<style scoped>
+.version-item { margin: 4px 0; }
+</style>
