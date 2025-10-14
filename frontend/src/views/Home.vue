71fffc4e569a7ba8188a9e4ae2249359ed7df8e7
tgszy <sunjinyuanqwe@gmail.com>
2025-10-14 09:06:12 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,1 +1,19 @@
+<template>
+  <div>
+    <h2>媒体库总览</h2>
+    <el-row :gutter="12">
+      <el-col v-for="m in media" :key="m.id" :span="8">
+        <VersionCard :media="m" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
 
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { mockList } from '@/api'
+import VersionCard from '@/components/VersionCard.vue'
+
+const media = ref([])
+onMounted(async () => { media.value = (await mockList()).data })
+</script>
