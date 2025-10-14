fe1996fa8b24c61c016c4af454df499ae8e9255e
tgszy <sunjinyuanqwe@gmail.com>
2025-10-14 12:53:13 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: { port: 5173, proxy: { '/api': 'http://localhost:8000' } }
+  server: { port: 5173, proxy: { '/api': 'http://localhost:35455' } }
 })
