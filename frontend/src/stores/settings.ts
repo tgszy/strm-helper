bd285f1e34bcec52d857be6e477ef142d07418a2
tgszy <sunjinyuanqwe@gmail.com>
2025-10-14 09:04:03 +0800

    Update settings.ts

--- frontend/src/stores/settings.ts
+++ frontend/src/stores/settings.ts
@@ -1,1 +1,12 @@
 
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import Settings from './views/Settings.vue'
+import Organize from './views/Organize.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/organize', component: Organize },
+  { path: '/settings', component: Settings },
+]
+export default createRouter({ history: createWebHistory(), routes })
