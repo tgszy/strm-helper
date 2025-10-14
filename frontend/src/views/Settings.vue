befb735ce608cc3e9e935ecfb289d2598148ea0b
tgszy <sunjinyuanqwe@gmail.com>
2025-10-14 10:44:29 +0800

    Update Settings.vue

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,11 +1,11 @@
 <template>
   <h2>高级设置</h2>
-  <el-form :model="settings" label-width="140px">
+  <el-form :model="form" label-width="140px">
     <el-form-item label="最大版本数">
-      <el-input-number v-model="settings.maxVersions" :min="1" :max="10"/>
+      <el-input-number v-model="form.maxVersions" :min="1" :max="10" />
     </el-form-item>
     <el-form-item label="命名模板">
-      <el-input v-model="settings.namingTemplate"/>
+      <el-input v-model="form.namingTemplate" />
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="save">保存</el-button>
@@ -14,7 +14,20 @@
 </template>
 
 <script setup lang="ts">
-import { useSettingsStore } from '@/stores/settings'
-const settings = useSettingsStore()
-function save() { ElMessage.success('已保存') }
+import { reactive, onMounted } from 'vue'
+import { ElMessage } from 'element-plus'
+import { getSettings, updateSettings } from '@/api'
+
+const form = reactive({ maxVersions: 3, namingTemplate: '' })
+
+onMounted(async () => {
+  const { data } = await getSettings()
+  form.maxVersions = data.max_versions
+  form.namingTemplate = data.naming_template
+})
+
+async function save() {
+  await updateSettings({ max_versions: form.maxVersions, naming_template: form.namingTemplate })
+  ElMessage.success('已保存')
+}
 </script>
