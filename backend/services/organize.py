0177dbc25e18099f74084c630c87769479e60fd8
tgszy <sunjinyuanqwe@gmail.com>
2025-10-14 08:51:44 +0800

    Update organize.py

--- backend/services/organize.py
+++ backend/services/organize.py
@@ -1,10 +1,11 @@
-from pathlib import Path
 import shutil
+from pathlib import Path
+from typing import Dict
 from .scanner import scan_strm
-from .rename import gen_target_path
+from .rename import gen_target_path, load_rules
 
 def organize(source_strm_root: Path, output_root: Path, rules_yaml: Path):
-    rules = load_rules(rules_yaml)
+    rules: Dict = load_rules(rules_yaml)
     for strm in scan_strm(source_strm_root):
         target = gen_target_path(strm, rules, output_root)
         shutil.move(strm, target)
